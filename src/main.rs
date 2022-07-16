40bfbb8c77580c2e2a48cf23a1ac7b1b7eb990e6
Wiktor Janecki <j.wiktor05@interia.pl>
2022-07-16 14:17:17 +0200

    Some refactoring

--- src/main.rs
+++ src/main.rs
@@ -1,114 +1,53 @@
 extern crate sdl2;
 
-use std::collections::HashMap;
-
 use sdl2::EventPump;
 use sdl2::event::Event;
-use sdl2::image::LoadTexture;
 use sdl2::keyboard::Keycode;
-use sdl2::pixels::Color;
-use sdl2::render::{Canvas, Texture, TextureCreator};
-use sdl2::video::{Window, WindowContext};
 
-struct RenderingState{
-    canvas: Canvas<Window>,
-    texture_creator: TextureCreator<WindowContext>,
-}
+mod render;
+use crate::render::*;
+
 struct InputState{
     event_pump: EventPump, 
     should_quit: bool,
 }
 
-type TilemapState = tiled::Map;
 
 pub fn main() -> Result<(), String> {
     let sdl_context = sdl2::init()?;
     let video_subsystem = sdl_context.video()?;
 
     let window = video_subsystem
-        .window("Project tomato", 1920,1080 )
+        .window("Project tomato", 1920, 1080)
         .position_centered()
-        //.opengl()
         .build()
         .map_err(|e| e.to_string())?;
 
     let mut canvas = window.into_canvas().build().map_err(|e| e.to_string())?;
     canvas.set_logical_size(320, 180).map_err(|e| e.to_string())?;
-    let texture_creator = canvas.texture_creator();
     let event_pump = sdl_context.event_pump()?;
 
-    let mut rendering_state = RenderingState{canvas, texture_creator};
+    let mut rendering_state = RenderingState::new(canvas);
     let mut input_state = InputState{event_pump, should_quit:false};
 
     let _pool = threadpool::ThreadPool::new(num_cpus::get());
 
     let mut loader = tiled::Loader::new();
     let mut start_map = loader.load_tmx_map("res/startmenu.tmx").unwrap();
-    
+    load_tilemap_to_textures(&mut rendering_state, &mut start_map);
     loop{
+        let frame_timer = std::time::Instant::now();
+
         input(&mut input_state);
         if input_state.should_quit { break };
-        
-        
 
         render(&mut rendering_state, &mut start_map);
+        
+        let frame_end_time = frame_timer.elapsed();
+        println!("{}", 1.0/frame_end_time.as_secs_f64());
     }
 
     Ok(())
-}
-
-fn render(state: &mut RenderingState, tile_state :&TilemapState){
-    let canvas = &mut state.canvas;
-    canvas.set_draw_color(Color::RGB(0, 0, 0));
-    canvas.clear();
-
-    // render tilemap
-    let mut textures: HashMap<String, Texture> = HashMap::new();
-    for tileset in tile_state.tilesets().to_owned(){
-        let image = tileset.image.as_ref().unwrap();
-        let path = image.source.as_os_str().to_str().unwrap();
-        let txt = state.texture_creator.load_texture(path).unwrap();
-        let name = tileset.name.clone();
-        textures.insert(name, txt);
-    }
-    for layer in tile_state.layers(){
-        match layer.layer_type(){
-            tiled::LayerType::TileLayer(tile_layer) => {
-                match tile_layer{
-                    tiled::TileLayer::Finite(tiles) => {
-                        let width = tiles.width();
-                        let height = tiles.height();
-                        for i in 0..height{
-                            for j in 0..width{
-                                if let Some(tile) = tiles.get_tile(j as i32, i as i32){
-                                    let x = tile.get_tile().unwrap();
-                                    let tilemap_id = x.tileset().name.clone();
-                                    let txt = textures.get(&tilemap_id).unwrap();
-
-                                    let tile_width = x.tileset().tile_width;
-                                    let tile_height = x.tileset().tile_height;
-
-                                    let dst = sdl2::rect::Rect::new((j*tile_width) as i32, (i*tile_height) as i32,tile_width,tile_height);
-                                    let x = tile.id() % (3); // 3 is tiles in tileset
-                                    let y = tile.id() / 3;
-                                    let src = sdl2::rect::Rect::new((x*tile_width) as i32, (y*tile_height) as i32, tile_width, tile_height);
-                                    // TODO: BŁAGAM NAPRAW TO
-                                    canvas.copy(txt, src, dst).unwrap();
-                                }
-                            }
-                        }
-                    },
-                    tiled::TileLayer::Infinite(_) => panic!("NOT IMPLEMENTED"),
-                }
-            },
-            tiled::LayerType::ObjectLayer(_) =>  panic!("NOT IMPLEMENTED"),
-            tiled::LayerType::ImageLayer(_) =>  panic!("NOT IMPLEMENTED"),
-            tiled::LayerType::GroupLayer(_) =>  panic!("NOT IMPLEMENTED"),
-        }
-    }
-
-
-    canvas.present();
 }
 
 fn input(state: &mut InputState){
---
+++ src/render.rs
@@ -0,0 +1,84 @@
+
+use std::collections::HashMap;
+
+
+use sdl2::image::LoadTexture;
+use sdl2::pixels::Color;
+use sdl2::render::Canvas;
+use sdl2::render::Texture;
+use sdl2::render::TextureCreator;
+use sdl2::video::Window;
+use sdl2::video::WindowContext;
+
+type TilemapState = tiled::Map;
+pub struct RenderingState{
+    canvas: Canvas<Window>,
+    texture_creator: TextureCreator<WindowContext>,
+    textures: HashMap<String, Texture>,
+}
+
+impl RenderingState{
+    pub fn new(canvas: Canvas<Window>) -> RenderingState{
+        let texture_creator = canvas.texture_creator();
+        RenderingState { canvas, texture_creator, textures: HashMap::new() }
+    }
+}
+
+
+pub fn load_tilemap_to_textures(state: &mut RenderingState, tile_state :&TilemapState){
+    for tileset in tile_state.tilesets().to_owned(){
+        let image = tileset.image.as_ref().unwrap();
+        let path = image.source.as_os_str().to_str().unwrap();
+        let txt = state.texture_creator.load_texture(path).unwrap();
+        let name = tileset.name.clone();
+        state.textures.insert(name, txt);
+    }
+}
+
+fn render_tilemap(state: &mut RenderingState, tile_state :&TilemapState){
+    for layer in tile_state.layers(){
+        match layer.layer_type(){
+            tiled::LayerType::TileLayer(tile_layer) => {
+                match tile_layer{
+                    tiled::TileLayer::Finite(tiles) => {
+                        let width = tiles.width();
+                        let height = tiles.height();
+                        for i in 0..height{
+                            for j in 0..width{
+                                if let Some(tile) = tiles.get_tile(j as i32, i as i32){
+                                    let x = tile.get_tile().unwrap();
+                                    let tilemap_id = x.tileset().name.clone();
+                                    let txt = state.textures.get(&tilemap_id).unwrap();
+
+                                    let tile_width = x.tileset().tile_width;
+                                    let tile_height = x.tileset().tile_height;
+
+                                    let dst = sdl2::rect::Rect::new((j*tile_width) as i32, (i*tile_height) as i32,tile_width,tile_height);
+                                    let x = tile.id() % (3); // 3 is tiles in tileset
+                                    let y = tile.id() / 3;
+                                    let src = sdl2::rect::Rect::new((x*tile_width) as i32, (y*tile_height) as i32, tile_width, tile_height);
+                                    // TODO: BŁAGAM NAPRAW TO
+                                    state.canvas.copy(txt, src, dst).unwrap();
+                                }
+                            }
+                        }
+                    },
+                    tiled::TileLayer::Infinite(_) => {},
+                }
+            },
+            tiled::LayerType::ObjectLayer(_) =>  {},
+            tiled::LayerType::ImageLayer(_) =>  {},
+            tiled::LayerType::GroupLayer(_) =>  {},
+        }
+    }
+}
+
+
+pub fn render(state: &mut RenderingState, tile_state :&TilemapState){
+    state.canvas.set_draw_color(Color::RGB(0, 0, 0));
+    state.canvas.clear();
+
+    render_tilemap(state,tile_state);
+
+    state.canvas.present();
+}
